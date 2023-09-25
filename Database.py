2cc1fe16465e876a38b215ed09bcb6267d9721b3
muffindud <ccatlabuga2002@gmail.com>
2023-09-25 10:41:59 +0300

    fix initial migration

--- Database.py
+++ Database.py
@@ -1,6 +1,8 @@
-import psycopg
+import sqlalchemy
 
 
 class Database:
     def __init__(self):
-        ...
+        self.db = sqlalchemy.create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+
+
