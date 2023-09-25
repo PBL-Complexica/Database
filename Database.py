752d3b2f73da8dd7002b489f5e8f801753566621
muffindud <ccatlabuga2002@gmail.com>
2023-09-25 12:02:30 +0300

    add basic inserts

--- Database.py
+++ Database.py
@@ -1,8 +1,41 @@
-import sqlalchemy
+import psycopg2
+from datetime import datetime
 
 
 class Database:
-    def __init__(self):
-        self.db = sqlalchemy.create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+    def __init__(self, host="localhost"):
+        self.db = psycopg2.connect(
+            host=host,
+            database="postgres",
+            user="postgres",
+            password="postgres"
+        )
+        self.cursor = self.db.cursor()
 
+    def insert_user(self, first_name, last_name, password_hash, birth_date=None):
+        self.cursor.execute(
+            "INSERT INTO app_user (first_name, last_name, password_hash, birth_date, created_at) VALUES (%s, %s, %s, %s, %s)",
+            (first_name, last_name, password_hash, birth_date, datetime.now())
+        )
+        self.db.commit()
 
+    def insert_email(self, email_address):
+        self.cursor.execute(
+            "INSERT INTO email_address (email_address, created_at) VALUES (%s, %s)",
+            (email_address, datetime.now())
+        )
+        self.db.commit()
+
+    def insert_phone(self, phone_number):
+        self.cursor.execute(
+            "INSERT INTO phone_number (phone_number, created_at) VALUES (%s, %s)",
+            (phone_number, datetime.now())
+        )
+        self.db.commit()
+
+    def insert_device(self, serial_number, device_name):
+        self.cursor.execute(
+            "INSERT INTO device (device_name, device_sn, created_at) VALUES (%s, %s, %s)",
+            (device_name, serial_number, datetime.now())
+        )
+        self.db.commit()
--- app.py
+++ app.py
@@ -15,34 +15,34 @@ class app_user(db.Model):
     last_name = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(), nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
 
 
 class email_address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email_address = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
 
 
 class user_email(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'), nullable=False)
     email_id = db.Column(db.Integer, db.ForeignKey('email_address.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
     removed_at = db.Column(db.DateTime, nullable=True, default='2100-01-01 00:00:00')
 
 
 class phone_number(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
 
 
 class user_phone(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'), nullable=False)
     phone_id = db.Column(db.Integer, db.ForeignKey('phone_number.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
     removed_at = db.Column(db.DateTime, nullable=True, default='2100-01-01 00:00:00')
 
 
@@ -50,47 +50,47 @@ class device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     device_name = db.Column(db.String(50), nullable=False)
     device_sn = db.Column(db.String(), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
 
 
 class user_device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'), nullable=False)
     device_id = db.Column(db.Integer, db.ForeignKey('device.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
     removed_at = db.Column(db.DateTime, nullable=True, default='2100-01-01 00:00:00')
 
 
 class category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
 
 
 class user_category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
     removed_at = db.Column(db.DateTime, nullable=True, default='2100-01-01 00:00:00')
 
 
 class subscription_type(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subscription_type_name = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
 
 
 class subscription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subscription_type_id = db.Column(db.Integer, db.ForeignKey('subscription_type.id'), nullable=False)
     valid_from = db.Column(db.DateTime, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
 
 
 class user_subscription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('app_user.id'), nullable=False)
     subscription_id = db.Column(db.Integer, db.ForeignKey('subscription.id'), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    created_at = db.Column(db.DateTime, nullable=False)
     removed_at = db.Column(db.DateTime, nullable=True, default='2100-01-01 00:00:00')
