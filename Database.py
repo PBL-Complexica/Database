1608c3c284c975cd93e1811f84257a306f9aa56e
muffindud <ccatlabuga2002@gmail.com>
2023-10-08 14:29:39 +0300

    add dotenv variables

--- Database.py
+++ Database.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from db_model import app
+from dotenv import dotenv_values
 import json
+import os
 
 
 class DatabaseMeta(type):
@@ -14,11 +16,13 @@ class DatabaseMeta(type):
 
 class Database(metaclass=DatabaseMeta):
     def __init__(self):
+        # Check if psycopg2 is installed
         try:
             import psycopg2
         except ImportError:
             raise ImportError("Please install psycopg2: 'pip install psycopg2'")
 
+        # Check if flask_migrate is installed
         try:
             from flask_migrate import upgrade
         except ImportError:
@@ -27,123 +31,164 @@ class Database(metaclass=DatabaseMeta):
         with app.app_context():
             upgrade()
 
-        try:
-            with open("config.json", "r") as f:
-                cfg = json.loads(f.read())
+        # Load environment variables
+        config = dotenv_values(".env")
+        self.db = psycopg2.connect(
+            host=config["DB_HOST"],
+            database=config["DB_DATABASE"],
+            user=config["DB_USER"],
+            password=config["DB_PASSWORD"]
+        )
+        self.cursor = self.db.cursor()
 
-                if any([cfg["database"]["host"], cfg["database"]["database"], cfg["database"]["user"], cfg["database"]["password"]]) == "":
-                    raise ValueError("Please fill in the database configuration file")
+        # Remove unconfirmed users
+        self.remove_unconfirmed()
 
-                self.db = psycopg2.connect(
-                    host=cfg["database"]["host"],
-                    database=cfg["database"]["database"],
-                    user=cfg["database"]["user"],
-                    password=cfg["database"]["password"]
-                )
-                self.cursor = self.db.cursor()
-        except FileNotFoundError:
-            open("config.json", "w").write(json.dumps({
-                "database": {
-                    "host": "",
-                    "database": "",
-                    "user": "",
-                    "password": ""
-                }
-            }, indent=4))
-            raise FileNotFoundError("Please fill in the database configuration file")
-
+        # Populate database with initial data
         self.__populate()
 
-    def custom_query(self, query, values: tuple):
-        self.cursor.execute(query, values)
-        self.db.commit()
-        if query.startswith("SELECT"):
-            return self.cursor.fetchall()
-
-    def insert_user(self, first_name, last_name, password_hash, birth_date=None):
+    # Private methods
+    # Insert new user
+    def __insert_user(self, first_name, last_name, password_hash, birth_date=None):
         self.cursor.execute(
             "INSERT INTO app_user (first_name, last_name, password_hash, birth_date, created_at) VALUES (%s, %s, %s, %s, %s)",
             (first_name, last_name, password_hash, birth_date, datetime.now())
         )
         self.db.commit()
 
-    def insert_email(self, email_address):
+    # Insert new email
+    def __insert_email(self, email_address):
         self.cursor.execute(
-            "INSERT INTO email_address (email_address, created_at) VALUES (%s, %s) ON CONFLICT DO NOTHING",
+            "INSERT INTO email_address (email_address, created_at) "
+            "VALUES (%s, %s) ON CONFLICT DO NOTHING",
             (email_address, datetime.now())
         )
         self.db.commit()
 
-    def insert_phone(self, phone_number):
+    # Insert new phone number
+    def __insert_phone(self, phone_number):
         self.cursor.execute(
-            "INSERT INTO phone_number (phone_number, created_at) VALUES (%s, %s) ON CONFLICT DO NOTHING",
+            "INSERT INTO phone_number (phone_number, created_at) "
+            "VALUES (%s, %s) ON CONFLICT DO NOTHING",
             (phone_number, datetime.now())
         )
         self.db.commit()
 
-    def insert_device(self, serial_number, device_name):
+    # Insert new device
+    def __insert_device(self, serial_number, device_name):
         self.cursor.execute(
-            "INSERT INTO device (device_name, device_sn, created_at) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING",
+            "INSERT INTO device (device_name, device_sn, created_at) "
+            "VALUES (%s, %s, %s) ON CONFLICT DO NOTHING",
             (device_name, serial_number, datetime.now())
         )
         self.db.commit()
 
-    def check_email(self, email_address, user_id):
+    # Return user's for a given active email address
+    def __check_email(self, email_address):
         self.cursor.execute(
-            "SELECT * FROM user_email WHERE email_id = (SELECT id FROM email_address WHERE email_address = %s) AND user_id = %s",
-            (email_address, user_id)
+            "SELECT user_id FROM user_email "
+            "WHERE email_id = (SELECT id FROM email_address WHERE email_address = %s) AND removed_at > %s",
+            (email_address, datetime.now())
         )
         return self.cursor.fetchall()
 
-    def check_phone(self, phone_number, user_id):
+    # Return user's for a given active phone number
+    def __check_phone(self, phone_number):
         self.cursor.execute(
-            "SELECT * FROM user_phone WHERE phone_id = (SELECT id FROM phone_number WHERE phone_number = %s) AND user_id = %s",
-            (phone_number, user_id)
+            "SELECT user_id FROM user_phone "
+            "WHERE created_at = (SELECT id FROM phone_number WHERE phone_number = %s) AND removed_at > %s",
+            (phone_number, datetime.now())
+        )
+        return self.cursor.fetchall()
+
+    # Return devices associated with a user device
+    def __check_device(self, serial_number):
+        self.cursor.execute(
+            "SELECT user_id FROM user_device "
+            "WHERE created_at = (SELECT id FROM device WHERE device_sn = %s) AND removed_at > %s",
+            (serial_number, datetime.now())
         )
         return self.cursor.fetchall()
 
     # Public methods
-    def register(self, first_name, last_name, password, email_address, device_name=None, phone_number=None, birth_date=None):
-        self.insert_user(first_name, last_name, password, birth_date)
+    # Remove unconfirmed users
+    def remove_unconfirmed(self):
         self.cursor.execute(
-            "SELECT id FROM app_user WHERE first_name = %s AND last_name = %s AND password_hash = %s AND birth_date = %s",
-            (first_name, last_name, password, birth_date)
+            "DELETE FROM app_user WHERE confirmed = FALSE"
         )
-        user_id = self.cursor.fetchone()[0]
-        self.insert_email(email_address)
-        self.insert_phone(phone_number)
-        # self.insert_device(device_name, device_name)
-        print(self.check_email(email_address, user_id))
-        print(self.check_phone(phone_number, user_id))
+        self.db.commit()
+
+    # Create new user and associate it with an email, phone number and device
+    # TODO: Finish
+    def register(
+            self,
+            first_name,
+            last_name,
+            password,
+            email_address,
+            device_name=None,
+            device_sn=None,
+            phone_number=None,
+            birth_date=None
+    ):
+        self.__insert_email(email_address)
+        self.__insert_phone(phone_number)
+        self.__insert_device(device_sn, device_name)
+
+        # Get new email's id
+        self.cursor.execute(
+            "SELECT id FROM email_address "
+            "WHERE email_address = %s",
+            (email_address,)
+        )
+        email_id = self.cursor.fetchone()[0] if email_address is not None else None
+
+        # Get new phone's id
+        self.cursor.execute(
+            "SELECT id FROM phone_number "
+            "WHERE phone_number = %s",
+            (phone_number,)
+        )
+        phone_id = self.cursor.fetchone()[0] if phone_number is not None else None
+
+        # Get new device's id
+        self.cursor.execute(
+            "SELECT id FROM device "
+            "WHERE device_sn = %s",
+            (device_sn,)
+        )
+        device_id = self.cursor.fetchone()[0] if device_sn is not None else None
 
     # Populate database with initial data
     def __add_categories(self, name: str, prices: list):
-        self.custom_query(
+        self.cursor.execute(
             "INSERT INTO subscription_type (subscription_type_name, months, cost, created_at) "
             "VALUES (%s, %s, %s, %s) "
             "ON CONFLICT DO NOTHING",
             (name + "-1", 1, prices[0], datetime.now())
         )
-        self.custom_query(
+        self.cursor.execute(
             "INSERT INTO subscription_type (subscription_type_name, months, cost, created_at) "
             "VALUES (%s, %s, %s, %s) "
             "ON CONFLICT DO NOTHING",
             (name + "-3", 3, prices[1], datetime.now())
         )
-        self.custom_query(
+        self.cursor.execute(
             "INSERT INTO subscription_type (subscription_type_name, months, cost, created_at) "
             "VALUES (%s, %s, %s, %s) O"
             "N CONFLICT DO NOTHING",
             (name + "-6", 6, prices[2], datetime.now())
         )
+        self.db.commit()
 
     def __add_user_categories(self, name: str):
-        self.custom_query(
+        self.cursor.execute(
             "INSERT INTO category (category_name, created_at) "
             "VALUES (%s, %s) "
             "ON CONFLICT DO NOTHING",
             (name, datetime.now())
         )
+        self.db.commit()
 
     def __populate(self):
         self.__add_categories("G", [234, 594, 972])
@@ -162,4 +207,3 @@ class Database(metaclass=DatabaseMeta):
         self.__add_user_categories("familie cu multi copii")
         self.__add_user_categories("personal didactic")
         self.__add_user_categories("personal medical")
-
--- db_model.py
+++ db_model.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import dotenv_values
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = dotenv_values(".env")["DB_URI"]
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
